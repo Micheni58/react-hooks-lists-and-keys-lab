996b772ce45639c568322ceb3032f61a78dfed7b
Micheni58 <michenicaleb@gmail.com>
2025-07-10 15:41:09 +0300

    Add rendering logic to display projects

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,21 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // const navLinks = links.map((link) =>{
+  //   return (
+  //     <a key={link}>{link}</a>
+  //   )
+  // })
+
+  return <nav>
+    {/* display an <a> tag for each link here */}
+   
+    {/* {navLinks} */}
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#projects">Projects</a>
+
+    </nav>;
 }
 
 export default NavBar;
